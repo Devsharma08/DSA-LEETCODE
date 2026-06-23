5d9672675e410f68f9958aa03d8768c91b6989d8
Devsharma08 <devsharma08072004@gmail.com>
2026-06-23 23:42:39 +0530

    [java] add shift 0's problem

---
+++ LeetCode-283E.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+
+        int insertPos = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[insertPos] = nums[i];
+                insertPos++;
+            }
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos] = 0;
+            insertPos++;
+        }
+    }
+}
